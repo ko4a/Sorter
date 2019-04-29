fb9aaed7f490729098c3074d87e5f9a847e2e0e5
ko4a <slavakochnevcase@gmail.com>
2019-04-29 22:12:03 +0700

    Добавил сортировку пузырьком

--- Sorter/Sort/BubbleSort.cs
+++ Sorter/Sort/BubbleSort.cs
@@ -2,11 +2,33 @@
 
 namespace Sorter.Sort
 {
-    class BubbleSort<T>:AlgorithmBase<T> where T:IComparable
+   public class BubbleSort<T>:AlgorithmBase<T> where T:IComparable
     {
         public override void Makesort()
         {
-            
+            if (Items.Count < 2)
+                return;
+
+            bool SwapIteration = false;
+
+            for (int i=1;i<Items.Count;i++)
+            {
+                var a = Items[i - 1];
+                var b = Items[i];
+                var tmpSwap = SwapCount;
+
+                if (a.CompareTo(b) > 0)
+                {
+                    Swap(i, i - 1);
+                    SwapIteration = true;
+                }
+                if (i == Items.Count - 1&& SwapIteration )
+                {
+                    SwapIteration = false;
+                    i = 0;
+                }
+
+            }
 
 
         }
