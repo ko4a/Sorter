b2c4720b1854b89e7e38794d577a624a03928cfb
ko4a <slavakochnevcase@gmail.com>
2019-04-30 22:13:28 +0700

    начал рефакторить тесты

---
+++ AlgorithmTest/SortTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace AlgorithmTest
+{
+    [TestClass]
+    public class SortTest
+    {
+        Random randomiser = new Random();
+        List<int> sorted = new List<int>();
+        List<int> items = new List<int>();
+
+        [TestInitialize]
+        public void Init()
+        {
+            items.Clear();
+
+            for (int i = 0; i < 10_000; i++)
+            {
+                items.Add(randomiser.Next());
+            }
+
+            sorted.Clear();
+            sorted.AddRange(items.OrderBy(x => x).ToArray());
+        }
+
+
+    }
+}
