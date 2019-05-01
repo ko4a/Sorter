d9a5bdf7e2e84701fe20de0247df92135721c3bd
ko4a <slavakochnevcase@gmail.com>
2019-05-01 14:08:02 +0700

    порефакторил тесты

--- AlgorithmTest/SortTest.cs
+++ AlgorithmTest/SortTest.cs
@@ -2,7 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-
+using Sorter.Sort;
 
 namespace AlgorithmTest
 {
@@ -27,6 +27,47 @@ namespace AlgorithmTest
             sorted.AddRange(items.OrderBy(x => x).ToArray());
         }
 
+
+        [TestMethod]
+        public void BaseSortTest()
+        {
+            var basesort = new AlgorithmBase<int>();
+            basesort.Items.AddRange(items);
+
+            basesort.Sort();
+
+            for (int i = 0; i < 10_000; i++)
+                Assert.AreEqual(sorted[i], basesort.Items[i]);
+
+        }
+
+
+        [TestMethod]
+        public void BubbleSortTest()
+        {
+            var bubble = new BubbleSort<int>();
+            bubble.Items.AddRange(items);
+
+            bubble.Sort();
+
+            for (int i = 0; i < 10_000; i++)
+                Assert.AreEqual(sorted[i], bubble.Items[i]);
+
+        }
+
+
+        [TestMethod]
+        public void CocktailSortTest()
+        {
+            var cocktail = new CocktailSort<int>();
+            cocktail.Items.AddRange(items);
+
+            cocktail.Sort();
+
+            for (int i = 0; i < 10_000; i++)
+                Assert.AreEqual(sorted[i], cocktail.Items[i]);
+
+        }
 
     }
 }
